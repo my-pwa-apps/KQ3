964c835d82c0385a6ddfcd7b5522d868ad896620
GarfieldApp <garfieldapp@outlook.com>
2025-03-10 20:39:14 +0100

    enhance interaction system; separate mouse and VR interaction setup

--- js/systems/interaction-system.js
+++ js/systems/interaction-system.js
@@ -1,20 +1,18 @@
 export class InteractionSystem {
     constructor(scene) {
         this.scene = scene;
-        this.setupInteraction();
+        this.setupMouseInteraction();
+        
+        // Wait for XR to be initialized before setting up VR controls
+        scene.onXRFrameObservable.add(() => {
+            if (!this.vrInitialized) {
+                this.setupVRInteraction();
+                this.vrInitialized = true;
+            }
+        });
     }
 
-    setupInteraction() {
-        // VR Controller interaction
-        this.scene.onXRControllerAdded.add((controller) => {
-            controller.onTriggerStateChangedObservable.add((state) => {
-                if (state.pressed) {
-                    this.handleInteraction(controller.position);
-                }
-            });
-        });
-
-        // Mouse interaction
+    setupMouseInteraction() {
         this.scene.onPointerDown = (evt) => {
             const pickResult = this.scene.pick(evt.clientX, evt.clientY);
             if (pickResult.hit) {
@@ -23,7 +21,25 @@ export class InteractionSystem {
         };
     }
 
+    setupVRInteraction() {
+        const xrHelper = this.scene.xrExperience;
+        if (xrHelper && xrHelper.input) {
+            xrHelper.input.onControllerAddedObservable.add((controller) => {
+                controller.onTriggerStateChangedObservable.add((state) => {
+                    if (state.pressed) {
+                        const ray = controller.getWorldPointerRayToRef();
+                        const pickInfo = this.scene.pickWithRay(ray);
+                        if (pickInfo.hit) {
+                            this.handleInteraction(pickInfo.pickedPoint);
+                        }
+                    }
+                });
+            });
+        }
+    }
+
     handleInteraction(position) {
-        // Handle object interactions
+        // Handle interactions here
+        console.log('Interaction at position:', position);
     }
 }
